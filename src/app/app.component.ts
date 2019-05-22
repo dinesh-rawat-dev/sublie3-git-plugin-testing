8cfc819861227d5851aad9d3d495c9c2af35bcdd
Dinesh Rawat <dineshrawat.mca@gmail.com>
2019-05-22 15:11:16 +0530

    This is tesitng by sublime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root'
 })
 export class AppComponent {
-  title = 'star-wars';
+	title = 'Hello world'
 }
